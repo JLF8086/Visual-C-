1b8eb6c7bc7f07d85d575dbc515b01247b307ee0
Tomas Vardanian <bfdenjoy@gmail.com>
2012-03-29 20:56:27 +0300

    -

--- File comparer/File comparer/Program.cs
+++ File comparer/File comparer/Program.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Diagnostics;
 
 namespace File_comparer
 {
-    static class Program
+    class Program
     {
         /// <summary>
         /// The main entry point for the application.
@@ -13,9 +17,33 @@ namespace File_comparer
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            /*Stopwatch sw = new Stopwatch();
+            sw.Start();
+            string s = GetMD5HashFromFile(@"D:\Downloads\mc7647500k.wmv");
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+            Console.WriteLine(s);*/
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            var files = Directory.GetFiles(@"D:\Downloads", "*", SearchOption.AllDirectories);
+            foreach (string str in files)
+                Console.WriteLine(str);
+        }
+
+        private static string GetMD5HashFromFile(string fileName)
+        {
+            FileStream file = new FileStream(fileName, FileMode.Open);
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] retVal = md5.ComputeHash(file);
+            file.Close();
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < retVal.Length; i++)
+            {
+                sb.Append(retVal[i].ToString("x2"));
+            }
+            return sb.ToString();
         }
     }
 }
